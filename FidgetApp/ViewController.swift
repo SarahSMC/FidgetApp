cbf7676712e0cb33f441907c9a9be29f2f60ab9e
SMC <039244@swcsd.us>
2021-04-19 14:05:34 -0400

    Put button in for next planned feature: currently researching how to get the feature to work

--- FidgetApp/ViewController.swift
+++ FidgetApp/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var ColorShift: UIView!
+    let colors: [UIColor] = [.black, .magenta, .red, .orange, .yellow, .green,.cyan, .blue, .purple,.brown]
+    var currentColor = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapColor(_ sender: UITapGestureRecognizer) {
+        if currentColor <= 8 {
+            currentColor += 1
+        } else {
+            currentColor = 0
+        }
 
+        ColorShift.backgroundColor = colors[currentColor]
+
+    }
+    
+    @IBAction func buzzButton(_ sender: UIButton) {
+        
+        //currently trying to reaserch how to pull off what this is planned to be.
+        
+    }
 }
 
