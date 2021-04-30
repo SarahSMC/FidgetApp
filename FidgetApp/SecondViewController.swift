2b0ef607b1fd9a9b3058ab04b5d26aae01444f4d
SMC <039244@swcsd.us>
2021-04-30 13:09:54 -0400

    Added second view, nav view, and segmented controller, have not added segue and features in second view as of now.

---
+++ FidgetApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  FidgetApp
+//
+//  Created by Sarah Mae Campbell on 4/30/21.
+//  Copyright © 2021 Sarah Mae Campbell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
